f8b57b3f5e4d9c5a1a080f943b930e8eb1b8292d
Bee <xronsredstone@gmail.com>
2023-03-14 20:28:04 +0000

    better?

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -10,13 +10,12 @@ const spellNumber = (num) => {
     const teens = ["", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"];
     const tens = ['', "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"];
     const bigNums = ['', "Hundred", 'Thousand', 'Million', 'Billion'];
-    if (num > 9999999999999) return "Number too large";
     if (num < 10) return ones[num];
     if (num < 20) return teens[num - 10];
     if (num < 100) return tens[Math.floor(num / 10)] + " " + ones[num % 10];
     if (num < 1000) return ones[Math.floor(num / 100)] + " " + bigNums[1] + " " + spellNumber(num % 100);
-    for (let i = 2; i < bigNums.length; i++)
-      if (num < 1000 ** i) return `${spellNumber(Math.floor(num / 1000 ** (i-1)))} ${bigNums[i]} ${spellNumber(num % 1000 ** (i-1))}`;
+    for (let i = 2; i < bigNums.length; i++) if (num < 1000 ** i) return `${spellNumber(Math.floor(num / 1000 ** (i-1)))} ${bigNums[i]} ${spellNumber(num % 1000 ** (i-1))}`;
+    else return ("Number out of range")
   }
 
 for (let i = 1; i <= maxNumber; i++) {
